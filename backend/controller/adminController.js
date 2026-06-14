845e9642f3b0a14ff797e74c13bdbab046376c47
adi-builds <adityapaul500@gmail.com>
2026-06-14 10:43:45 +0530

    added admin in backend

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import fs from 'fs'
-
+import jwt from 'jsonwebtoken'
 //api for adding doctor
 const addDoctor = async(req,res)=>{
 
@@ -62,5 +62,20 @@ const addDoctor = async(req,res)=>{
         res.json({success:false, message:error.message})
     }
 }
+//API for admin login
+const loginAdmin = async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }else{
+            res.json({success:false, message:'Invalid credentials'})
+        }
+    }catch(error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
 
-export {addDoctor};
+export {addDoctor, loginAdmin};
