76ebcde68df77272c7d723ed827bf3dc4367d839
adi-builds <adityapaul500@gmail.com>
2026-06-17 13:29:44 +0530

    last one was appointments ts one is dashboard and complete

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -10,6 +10,7 @@ const DoctorContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [dToken,setDToken] = useState(localStorage.getItem('dToken')? localStorage.getItem('dToken'):'')
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const getAppointments = async()=>{
         try {
@@ -58,10 +59,25 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getDashData = async ()=>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/dashboard', {headers:{dToken}})
+            if(data.success){
+                setDashData(data.dashData)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         dToken,setDToken,
         backendUrl,getAppointments,appointments,
-        setAppointments,completAppointment,cancelAppointment
+        setAppointments,completAppointment,cancelAppointment,
+        getDashData,dashData,setDashData
     }
 
     return (
--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { doctorList, loginDoctor, appointmentsDoctor,appointmentCancelled,appointmentCompleted } from '../controller/doctorController.js'
+import { doctorList, loginDoctor, appointmentsDoctor,appointmentCancelled,appointmentCompleted,doctorDashboard } from '../controller/doctorController.js'
 import authDoctor from '../middleware/authDoctor.js'
 
 const doctorRouter = express.Router()
 
 doctorRouter.get('/list', doctorList);
 doctorRouter.get('/appointments', authDoctor, appointmentsDoctor);
+doctorRouter.get('/dashboard', authDoctor, doctorDashboard);
 doctorRouter.post('/login', loginDoctor);
 doctorRouter.post('/cancel-appointment',authDoctor,appointmentCancelled);
 doctorRouter.post('/complete-appointment',authDoctor,appointmentCompleted);
